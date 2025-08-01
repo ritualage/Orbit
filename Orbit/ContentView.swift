aa663149b66f8a72baef8b7e81226bb0f1ead770
Ritual Age <78867258+ritualage@users.noreply.github.com>
2025-08-01 08:24:45 -0600

    Initial Commit

---
+++ Orbit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orbit
+//
+//  Created by Patrick Moran on 8/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by Patrick Moran on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
